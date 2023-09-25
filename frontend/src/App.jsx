663c140b8475b36663e138499cd1fe36c25758ef
Vishal Majumdar <vishalmajumdar1@gmail.com>
2023-09-26 02:18:49 +0530

    Refactor code
    2 components were removed as they were not ready and there was problem in deployment but not in local

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,10 @@
 import "./App.css";
-import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Home from "./components/Home";
 
 function App() {
   return (
     <>
       <Header></Header>
-      <Home></Home>
-      <Footer></Footer>
     </>
   );
 }
