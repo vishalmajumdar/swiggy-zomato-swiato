5ada03ec4b7d6921918056742b96084f84db001a
Vishal Majumdar <vishalmajumdar1@gmail.com>
2023-09-27 11:27:49 +0530

    Troubleshoot Layout Shift
    There was layout shift due to scroll on different pages so implemented a solution

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
+        <Footer />
       </Router>
     </>
   );
