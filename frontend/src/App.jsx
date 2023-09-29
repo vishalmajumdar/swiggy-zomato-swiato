d53395b3702672b9ab5eb5f2952730e71bfb2914
Vishal Majumdar <vishalmajumdar1@gmail.com>
2023-09-29 13:38:54 +0530

    Pass allRestaurants using createContext
    instead of using props drilling we are using createcontext and useContext Hooks from react which makes the code shorter and helps to declare global variables

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 // This is the main file and the first import is used for Routing or calling different URLs and getting different UIs
+import { createContext, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Restaurants from "./pages/Restaurants";
@@ -6,20 +7,26 @@ import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import getAllRestaurants from "./data/restaurants.json";
+
+export const AppContext = createContext();
 
 function App() {
+  const [allRestaurants, setAllRestaurants] = useState(getAllRestaurants);
   return (
     <>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/restaurants" element={<Restaurants />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        <Footer />
-      </Router>
+      <AppContext.Provider value={{ allRestaurants }}>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/restaurants" element={<Restaurants />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </AppContext.Provider>
     </>
   );
 }
--- frontend/src/components/AllRestaurants.jsx
+++ frontend/src/components/AllRestaurants.jsx
@@ -1,12 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState, useContext } from "react";
 import { RestaurantCard } from "./RestaurantCard";
 import { Typography, Button, Spinner } from "@material-tailwind/react";
-import restaurantsData from "../data/restaurants.json";
+import { AppContext } from "../App";
 
 const AllRestaurants = () => {
-  const [restaurants, setRestaurants] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const { allRestaurants } = useContext(AppContext);
+
   const [visibleRestaurants, setVisibleRestaurants] = useState(6); // Initial number of visible restaurants
 
   const [showSpinner, setShowSpinner] = useState(false); // State to control spinner visibility
@@ -17,27 +16,6 @@ const AllRestaurants = () => {
       (prevVisibleRestaurants) => prevVisibleRestaurants + 6
     );
   };
-
-  useEffect(() => {
-    // Fetch the JSON data
-    fetch("/restaurants.json")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json(); // Parse the response body as JSON
-      })
-      .then((data) => {
-        // Update state with the fetched data
-        setRestaurants(data);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        // Handle any errors that occurred during fetch
-        setError(error);
-        setIsLoading(false);
-      });
-  }, []);
 
   // Function to fetch more restaurants
   const fetchMoreRestaurants = () => {
@@ -51,32 +29,6 @@ const AllRestaurants = () => {
     }, 750); // Adjust the duration as needed (in milliseconds)
   };
 
-  if (isLoading) {
-    return (
-      <div className="m-12 flex flex-col items-center">
-        <Typography
-          variant="h2"
-          className="font-playfairDisplay mb-[60px] text-white bg-theme-color text-center px-4 py-2 rounded-xl shadow-lg mx-3 text-xl sm:text-3xl md:text-4xl"
-        >
-          Loading...
-        </Typography>
-      </div>
-    );
-  }
-
-  if (error) {
-    return (
-      <div className="m-12 flex flex-col items-center">
-        <Typography
-          variant="h2"
-          className="font-playfairDisplay mb-[60px] text-white bg-theme-color text-center px-4 py-2 rounded-xl shadow-lg mx-3 text-xl sm:text-3xl md:text-4xl"
-        >
-          Error: {error.message}
-        </Typography>
-      </div>
-    );
-  }
-
   return (
     <div className="m-12 flex flex-col items-center">
       <Typography
@@ -86,7 +38,7 @@ const AllRestaurants = () => {
         Feast Your Eyes on Choices
       </Typography>
       <div className="flex flex-wrap gap-8 justify-center">
-        {restaurantsData.slice(0, visibleRestaurants).map((restaurant) => (
+        {allRestaurants.slice(0, visibleRestaurants).map((restaurant) => (
           <div
             className="basis-[100%] sm:basis-[40%] lg:basis-[30%]"
             key={restaurant.id}
@@ -97,7 +49,7 @@ const AllRestaurants = () => {
             />
           </div>
         ))}
-        {visibleRestaurants < restaurants.length && (
+        {visibleRestaurants < allRestaurants.length && (
           <div className="mt-4">
             <Button
               className="font-poppins bg-theme-color"
